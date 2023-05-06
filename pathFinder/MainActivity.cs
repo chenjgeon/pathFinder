cd6f0bc5eeb63dc35696ed2125aac22eef7c50c0
ACT-STUDENT <ACT-STUDENT@3D-PC23>
2023-05-06 14:50:20 +0800

    Add project files.

---
+++ pathFinder/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android;
+using Android.App;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+
+namespace pathFinder
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+        GoogleMap map;
+
+        readonly string[] permissionGroup =
+        {
+            Manifest.Permission.AccessCoarseLocation, Manifest.Permission.AccessFineLocation
+        };
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            RequestPermissions(permissionGroup, 0);
+
+            //references
+            SupportMapFragment mapFragment = (SupportMapFragment)SupportFragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
+        }
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            var mapStyle = MapStyleOptions
+                .LoadRawResourceStyle(this, Resource.Drawable.mapper);
+            googleMap.SetMapStyle(mapStyle);
+            map = googleMap;
+
+            map.UiSettings.ZoomControlsEnabled = true;
+        }
+    }
+}
